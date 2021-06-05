417f0f6c79b4134819beee7d0bcf14ff7b0d4518
Matthew Yong <matthew.yong0801@gmail.com>
2021-06-05 01:24:18 -0700

    Added fruits

--- source/main.c
+++ source/main.c
@@ -28,18 +28,52 @@ typedef struct _task{
 
 unsigned char numRows = 5;
 unsigned char numCols = 8;
+unsigned char i;
+
+unsigned char fruitEaten = 0;
+unsigned char fruitCol = 0;
+unsigned char fruitRow = 0;
+enum fruitStates{Fruit_wait, Fruit_eaten};
+int TickFruit(int state){
+	switch(state){
+		case Fruit_wait:
+			if (fruitEaten){
+				state = Fruit_eaten;
+				fruitEaten = 0;
+			}
+			break;
+		case Fruit_eaten:
+			fruitRow = rand() % numRows;
+			fruitCol = rand() % numCols;
+			state = Fruit_wait;
+			break;
+		default:
+			fruitRow = rand() % numRows;
+			fruitCol = rand() % numCols;
+			fruitEaten = 0;
+			state = Fruit_wait;
+			break;
+	}
+	return state;
+}
+
 int snakePos[40][2];
 unsigned char snakeLength;
 unsigned char direction = 1; //0 = up, 1 = down, 2 = right, 3 = left;
 unsigned char gameOver = 0;
-enum snakeStates{Snake_start, Snake_move, Snake_eat, Snake_end}state;
+enum snakeStates{Snake_start, Snake_move, Snake_end}state;
 int TickSnake(int state){
 	switch(state){
 		case Snake_start:
 			state = Snake_move;
 			break;
 		case Snake_move:
-			for (unsigned char i=snakeLength-1; i > 0; i--){ //rightshift array
+			if (snakePos[0][0] == fruitRow && snakePos[0][1] == fruitCol){
+				fruitEaten = 1;
+				snakeLength++;
+			}
+
+			for (i=snakeLength-1; i > 0; i--){ //rightshift array
 				snakePos[i][0] = snakePos[i-1][0];
 				snakePos[i][1] = snakePos[i-1][1];
 			}
@@ -56,7 +90,7 @@ int TickSnake(int state){
 					state = Snake_end;
 				}
 				else{
-					snakePos[0][0]++;; //move down
+					snakePos[0][0]++; //move down
 				}
 			}
 			else if(direction == 2){
@@ -76,8 +110,6 @@ int TickSnake(int state){
 				}
 			}
 			break;
-		case Snake_eat:
-			break;
 		case Snake_end:
 			break;
 		default:
@@ -93,8 +125,6 @@ int TickSnake(int state){
 			gameOver = 0;
 			break;
 		case Snake_move:
-			break;
-		case Snake_eat:
 			break;
 		case Snake_end:
 			gameOver = 1;
@@ -153,64 +183,109 @@ int TickMove(int state){
 unsigned char pattern = 0x00;
 unsigned char row = 0xFF;
 unsigned char one = 0x01;
-unsigned char zero = 0xFE;
-unsigned char countPos = 0;
-enum LightStates{Light_on, Light_end};
+unsigned char j;
+enum LightStates{Light_row0, Light_row1, Light_row2, Light_row3, Light_row4, Light_end};
 int TickLights(int state){
 	switch (state){
-		case Light_on:
+		case Light_row0:
 			if (gameOver){
 				state = Light_end;
 			}
 			pattern = 0x00;
-			row = 0xFF;
-			for (unsigned char i=0; i < snakeLength; i++){
-				one = 0x80;
-				zero = 0xFE;
-				countPos = snakePos[i][0];
-				while (countPos > 0){
-					zero = zero << 1;
-					zero += 1;
-					countPos--;
-				}
-				row &= zero;
-				pattern |= one >> snakePos[i][1];
+			row = 0xFE;
+			one = 0x80;
+			if (fruitRow == 0){
+				pattern |= one >> fruitCol;
 			}
+			for (j=0; j < snakeLength; j++){
+				if (snakePos[j][0] == 0){
+					one = 0x80;
+					pattern |= one >> snakePos[j][1];
+				}
+			}
+			state = Light_row1;
+			break;
+		case Light_row1:
+			if (gameOver){
+				state = Light_end;
+			}
+			pattern = 0x00;
+			row = 0xFD;
+			one = 0x80;
+			if (fruitRow == 1){
+				pattern |= one >> fruitCol;
+			}
+			for (j=0; j < snakeLength; j++){
+				if (snakePos[j][0] == 1){
+					one = 0x80;
+					pattern |= one >> snakePos[j][1];
+				}
+			}
+			state = Light_row2;
+			break;
+		case Light_row2:
+			if (gameOver){
+				state = Light_end;
+			}
+			pattern = 0x00;
+			row = 0xFB;
+			one = 0x80;
+			if (fruitRow == 2){
+				pattern |= one >> fruitCol;
+			}
+			for (j=0; j < snakeLength; j++){
+				if (snakePos[j][0] == 2){
+					one = 0x80;
+					pattern |= one >> snakePos[j][1];
+				}
+			}
+			state = Light_row3;
+			break;
+		case Light_row3:
+			if (gameOver){
+				state = Light_end;
+			}
+			pattern = 0x00;
+			row = 0xF7;
+			one = 0x80;
+			if (fruitRow == 3){
+				pattern |= one >> fruitCol;
+			}
+			for (j=0; j < snakeLength; j++){
+				if (snakePos[j][0] == 3){
+					one = 0x80;
+					pattern |= one >> snakePos[j][1];
+				}
+			}
+			state = Light_row4;
+			break;
+		case Light_row4:
+			if (gameOver){
+				state = Light_end;
+			}
+			pattern = 0x00;
+			row = 0xEF;
+			one = 0x80;
+			if (fruitRow == 4){
+				pattern |= one >> fruitCol;
+			}
+			for (j=0; j < snakeLength; j++){
+				if (snakePos[j][0] == 4){
+					one = 0x80;
+					pattern |= one >> snakePos[j][1];
+				}
+			}
+			state = Light_row0;
 			break;
 		case Light_end:
 			pattern = 0xFF;
 			row = 0xEE;
 			break;
 		default:
-			state = Light_on;
+			state = Light_row0;
 	}
 	PORTC = pattern;
 	PORTD = row;
-	return state;
-}
-
-enum GameStates{Game_start, Game_play, Game_end};
-int TickGame(int state){
-	switch (state){
-		case Game_start:
-			state = Game_play;
-			break;
-		case Game_play:
-			break;
-		case Game_end:
-			break;
-		default:
-			state = Game_start;
-			break;
-	}
-	switch (state){
-		case Game_start:
-			break;
-		case Game_play:
-			break;
-		case Game_end:
-			break;
-	}
 	return state;
 }
 
@@ -221,16 +296,16 @@ int main(void) {
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
-	unsigned int GCD = 100;
+	unsigned int GCD = 1;
 	TimerSet(GCD);
 	TimerOn();
-	static task task1, task2, task3;
-	task *tasks[] = {&task1, &task2, &task3};
+	static task task1, task2, task3, task4;
+	task *tasks[] = {&task1, &task2, &task3, &task4};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 	const char start = -1;
 
 	task1.state = start;
-	task1.period = 700;
+	task1.period = 600;
 	task1.elapsedTime = task1.period;
 	task1.TickFct = &TickSnake;
 
@@ -240,9 +315,14 @@ int main(void) {
 	task2.TickFct = &TickMove;
 
 	task3.state = start;
-	task3.period = 700;
+	task3.period = 600;
 	task3.elapsedTime = task3.period;
-	task3.TickFct = &TickLights;
+	task3.TickFct = &TickFruit;
+
+	task4.state = start;
+	task4.period = 1;
+	task4.elapsedTime = task4.period;
+	task4.TickFct = &TickLights;
 
     while (1) {
 	for (unsigned char i=0; i < numTasks; i++){
